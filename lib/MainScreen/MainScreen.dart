30b49530229de34cc860f5e082860c69a45aaf42
AbdurRahmanasim <abdurrahmanasim7@gmail.com>
2023-08-10 11:30:38 +0500

    in progress

--- lib/MainScreen/MainScreen.dart
+++ lib/MainScreen/MainScreen.dart
@@ -60,90 +60,94 @@ class _MainScreenState extends State<MainScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("WhatsApp"),
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            PopupMenuButton(
-              onSelected: (value) {
-                // if (value == "Edit User") {
-                //   Get.to(EditUserPage());
-                // } else if (x == "Login History") {
-                //   Get.to(EditUserPage());
-                // } else if (x == "Change Password") {
-                //   Get.to(EditUserPage());
-                // } else if (x == "Deactivate User") {
-                //   Get.to(EditUserPage());
-                // }
-                // print(value);
-                // print(value);
-                // print(value);
-                if (value == 4) {
-                  Navigator.pushNamed(
-                    context,
-                    'setting',
-                  );
-                  // Navigator.of(context).push(MaterialPageRoute(
-                  //     builder: (context) => const Settings()));
-                }
-              },
-              itemBuilder: (context) => const [
-                PopupMenuItem(
-                  value: 0,
-                  child: Text("New Group"),
-                ),
-                PopupMenuItem(
-                  value: 1,
-                  child: Text("New broadcast"),
-                ),
-                PopupMenuItem(
-                  value: 2,
-                  child: Text("Linked Devices"),
-                ),
-                PopupMenuItem(
-                  value: 3,
-                  child: Text("Starred messages"),
-                ),
-                PopupMenuItem(
-                  value: 4,
-                  child: Text("Setting"),
-                  // onTap: () {
-                  //   Navigator.of(context).push(MaterialPageRoute(
-                  //     builder: (context) => Settings(),
-                  //   ));
-                  // },
-                ),
-              ],
-            )
-          ],
-          bottom: TabBar(
-            tabs: topTabs,
-            controller: _tabController,
-            indicatorColor: Colors.white,
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 1,
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("WhatsApp"),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              PopupMenuButton(
+                onSelected: (value) {
+                  // if (value == "Edit User") {
+                  //   Get.to(EditUserPage());
+                  // } else if (x == "Login History") {
+                  //   Get.to(EditUserPage());
+                  // } else if (x == "Change Password") {
+                  //   Get.to(EditUserPage());
+                  // } else if (x == "Deactivate User") {
+                  //   Get.to(EditUserPage());
+                  // }
+                  // print(value);
+                  // print(value);
+                  // print(value);
+                  if (value == 4) {
+                    Navigator.pushNamed(
+                      context,
+                      'setting',
+                    );
+                    // Navigator.of(context).push(MaterialPageRoute(
+                    //     builder: (context) => const Settings()));
+                  }
+                },
+                itemBuilder: (context) => const [
+                  PopupMenuItem(
+                    value: 0,
+                    child: Text("New Group"),
+                  ),
+                  PopupMenuItem(
+                    value: 1,
+                    child: Text("New broadcast"),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: Text("Linked Devices"),
+                  ),
+                  PopupMenuItem(
+                    value: 3,
+                    child: Text("Starred messages"),
+                  ),
+                  PopupMenuItem(
+                    value: 4,
+                    child: Text("Setting"),
+                    // onTap: () {
+                    //   Navigator.of(context).push(MaterialPageRoute(
+                    //     builder: (context) => Settings(),
+                    //   ));
+                    // },
+                  ),
+                ],
+              )
+            ],
+            bottom: TabBar(
+              tabs: topTabs,
+              controller: _tabController,
+              indicatorColor: Colors.white,
+            ),
           ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: const [
-            Center(
-                child: Text(
-              "Community",
-              style: TextStyle(fontSize: 50),
-            )),
-            Chats(),
-            Center(
-                child: Text(
-              "Status",
-              style: TextStyle(fontSize: 50),
-            )),
-            Center(
-                child: Text(
-              "Calls",
-              style: TextStyle(fontSize: 50),
-            )),
-          ],
-        ));
+          body: TabBarView(
+            controller: _tabController,
+            children: const [
+              Center(
+                  child: Text(
+                "Community",
+                style: TextStyle(fontSize: 50),
+              )),
+              Chats(),
+              Center(
+                  child: Text(
+                "Status",
+                style: TextStyle(fontSize: 50),
+              )),
+              Center(
+                  child: Text(
+                "Calls",
+                style: TextStyle(fontSize: 50),
+              )),
+            ],
+          )),
+    );
   }
 }
